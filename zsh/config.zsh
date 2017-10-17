d60eb2b70f0a5b56495446be0abeb5d7b4d7575e
donbader <qko589999@gmail.com>
2017-10-18 00:29:30 +0800

    zsh prompt -> bullet-train, and also install oh-my-zsh

---
+++ zsh/config.zsh
@@ -0,0 +1,15 @@
+# Path to your oh-my-zsh installation.
+export ZSH="$(dirname $0)/oh-my-zsh"
+
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(git)
+source $ZSH/oh-my-zsh.sh
+
+# For bullet-train prompt custom segments
+# See https://github.com/caiogondim/bullet-train.zsh#prompt
+
+# hide context user@hostname
+export BULLETTRAIN_CONTEXT_DEFAULT_USER=`whoami`
---
+++ zsh/configure.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Make ZSH as the default shell for the current user
+sudo chsh -s /bin/zsh "`whoami`"
+
+# Load all the zsh files in the ~/.zshrc
+echo "# dotfiles zsh config" > ~/.zshrc
+echo "source `pwd`/$(dirname $0)/main.zsh" >> ~/.zshrc
+
+# Create basic folders
+mkdir -p ~/bin
+mkdir -p ~/tmp
+
+mkdir -p ~/Documents/bin
+mkdir -p ~/Documents/work
