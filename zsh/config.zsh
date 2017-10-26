5bac1e253eaf29b702371bc10745a83a732980b6
donbader <qko589999@gmail.com>
2017-10-26 16:24:49 +0800

    vim can navigate window in insert mode

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -12,11 +12,14 @@ export ZSH="$(dirname $0)/oh-my-zsh"
 # Never auto apply the theme
 ZSH_THEME=""
 
+
+
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git task)
+plugins=(git task extract)
+
 source $ZSH/oh-my-zsh.sh
 
 
