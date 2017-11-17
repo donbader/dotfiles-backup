f1b3af27f10f6f1b0ea5f91fd4aa4cc2c0551fb9
donbader <qko589999@gmail.com>
2017-11-17 17:12:19 +0800

    ruby config

--- ruby/configure.sh
+++ ruby/configure.sh
@@ -1,3 +1,7 @@
 \curl -sSL https://get.rvm.io | bash -s stable
 echo "[[ -s \"$HOME/.rvm/scripts/rvm\" ]] && . \"$HOME/.rvm/scripts/rvm\"" >> ~/.zshrc 
+echo "source $HOME/.rvm/scripts/rvm" >> ~/.zshrc
 
+rvm install 2.3.3
+
+sudo gem install rails
