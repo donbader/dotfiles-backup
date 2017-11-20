0ecfeef43866dac0db7d4ba601e516d114fc4d17
donbader <qko589999@gmail.com>
2017-11-20 11:15:42 +0800

    [vscode] user settings refactor

--- macos/apps/vscode.sh
+++ macos/apps/vscode.sh
@@ -1,6 +1,8 @@
 # Link
-ln -sf `pwd`/$(dirname $0)/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
-ln -sf `pwd`/$(dirname $0)/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -sf `pwd`/$(dirname $0)/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
+# ln -sf `pwd`/$(dirname $0)/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+# ln -sf `pwd`/$(dirname $0)/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+# ln -sf `pwd`/$(dirname $0)/vscode/snippets ~/Library/Application\ Support/Code/User/snippets
+# ln -sf `pwd`/$(dirname $0)/vscode/projects.json ~/Library/Application\ Support/Code/User/projects.json
 
-
+rm -rf ~/Library/Application\ Support/Code/User
+ln -sf `pwd`/$(dirname $0)/vscode/User ~/Library/Application\ Support/Code
